e968d9b1a12679a2691ac651cf7a9c018b980190
Hakan Bilgin <hbi99@hotmail.com>
2021-02-07 12:01:15 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -9,36 +9,34 @@ const manual = {
 		sideBar.init(manual, contentView);
 		contentView.init(manual, sideBar);
 	},
-	async dispatch(event) {
+	dispatch(event) {
 		let file;
 		switch (event.type) {
 			case "open.file":
-				if (event.text) {
-					file = event;
-				} else {
-					file = await event.open();
-				}
-				if (file.text.slice(0,5).toLowerCase() === "[toc]") {
-					// shows sidebar toggler in the toolbar
-					window.find(".tool-sidebar-toogle").show();
+				event.open({ responseType: "text" })
+					.then(file => {
+						if (file.text.slice(0,5).toLowerCase() === "[toc]") {
+							// shows sidebar toggler in the toolbar
+							window.find(".tool-sidebar-toogle").show();
 
-					// sidebar parse toc
-					sideBar.dispatch({
-						type: "parse-toc",
-						path: file.path,
-						text: file.text,
-					});
-				} else {
-					// hides sidebar toggler in the toolbar
-					window.find(".tool-sidebar-toogle").hide();
+							// sidebar parse toc
+							sideBar.dispatch({
+								type: "parse-toc",
+								path: file.path,
+								text: file.text,
+							});
+						} else {
+							// hides sidebar toggler in the toolbar
+							window.find(".tool-sidebar-toogle").hide();
 
-					// sidebar parse file
-					contentView.dispatch({
-						type: "parse-markdown",
-						text: file.text,
-						path: file.path,
+							// sidebar parse file
+							contentView.dispatch({
+								type: "parse-markdown",
+								text: file.text,
+								path: file.path,
+							});
+						}
 					});
-				}
 				break;
 			case "sidebar-toggle-view":
 			case "sidebar-select-article":
